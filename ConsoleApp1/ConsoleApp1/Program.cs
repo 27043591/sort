cd7be40fc9edd645c5c883903e0387bb1921d884
Maheshwar Maheshwar <27043591@student.toiohomai.ac.nz>
2019-05-30 13:44:31 +1200

    sort

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            int[] nums = new int[100000];
+
+            Random rand = new Random();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rand.Next (1,100001);
+            }
+            InsertionSort(arr);
+            ImprovedBubbleSort(arr);
+
+        }
+        static void InsertionSort(int[] _arr)
+        {
+            int[] arr = new int[10];
+            Stopwatch InsertionSortStopwatch = new Stopwatch();
+            int temp;
+            int j;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                j = i;
+                temp = arr[i];
+                while (j > 0 && arr[j - 1] >= temp)
+                {
+                    arr[j] = arr[j - 1];
+                    j -= 1;
+                }
+                arr[j] = temp;
+            }
+
+            foreach (int x in arr)
+                Console.WriteLine(x);
+            Console.ReadLine();
+        }
+        static void ImprovedBubbleSort(int[] _arr)
+        {
+            Stopwatch BubbleSortStopwatch = new Stopwatch();
+            for (int i =0; i < Array<)
+        }
+    }
+}
---
+++ selection sort/selection sort/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selection_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10000];
+            Random rand = new Random();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rand.Next(1, 101);
+            }
+
+            Console.WriteLine("******** Selection sort Algorithm********");
+            Console.WriteLine("Orignal Datset:");
+            Display(nums);
+            InsertionSort(nums);
+            
+            SelectionSort(nums);
+            BubbleSort(nums);
+            Display(nums);
+        }
+        static void SelectionSort(int[] nums) { 
+            for (int outter = 0; outter < nums.Length; outter++)
+            {
+                int min = outter;
+                for (int inner = outter + 1; inner < nums.Length; inner++)
+                {
+                    if (nums[inner] < nums[min])
+                        min = inner;
+                }
+                int temp = nums[outter];
+                nums[outter] = nums[min];
+                nums[min] = temp;
+            }
+
+            Console.WriteLine("\n Sorted Datset:");
+            Display(nums);
+
+        }
+       
+        static void Display(int[] nums)
+        {
+            foreach (int x in nums)
+                Console.Write(x + "\t");
+
+            Console.WriteLine();
+           Console.ReadLine();
+        }
+        static void InsertionSort(int[] _nums)
+        {
+            int temp;
+            int j;
+            for (int i =1; i< _nums.Length; i++)
+            {
+                j = i;
+                temp = _nums[i];
+                while (j > 0 && _nums[j - 1] >= temp)
+                {
+                    _nums[j] = _nums[j - 1];
+                    j -= 1;
+                }
+                _nums[j] = temp;
+            }
+            foreach (int x in _nums)
+                Console.WriteLine(x + "\t");
+            Console.ReadLine();
+        }
+        static void BubbleSort( int[] _nums)
+        {
+            
+           
+            for ( int i = 0; i< _nums.Length -1; i++)
+            {
+                for (int j = 0; j < _nums.Length - 1; j++)
+                {
+                    if (_nums[j]> _nums[j + 1])
+                    {
+                        int temp = _nums[j + 1];
+                        _nums[j + 1] = _nums[j];
+                        _nums[j] = temp;
+                    }
+                }
+
+            }
+        }
+    }
+}
